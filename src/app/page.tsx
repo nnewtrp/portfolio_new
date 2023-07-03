ca03e9a6bb361641d2fc13d45241d726b83bc92e
zombiedude91 <74162594+zombiedude91@users.noreply.github.com>
2023-07-03 23:31:26 +0700

    update text styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,15 @@ export default function Home() {
         <div className="bg-black text-white text-2xl px-5 py-3">New</div>
 
         <div className="text-2xl px-36 py-16">
-          <div className="text-black font-bold text-5xl">
+          <h1 className="text-black font-bold text-5xl">
             Hi, I'm
-          </div>
-          <div className="text-black font-bold text-8xl mt-4">
+          </h1>
+          <h1 className="text-black font-bold text-8xl mt-4">
             Teerapat Satitporn.
-          </div>
-          <div className="text-black font-bold text-5xl mt-48">
+          </h1>
+          <h2 className="text-black font-bold text-5xl mt-48">
             I am a web developer.
-          </div>
+          </h2>
         </div>
       </div>
     </>
