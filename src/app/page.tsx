43f5668ad1a3aafe4e00359de2d6da7efd8251bd
zombiedude91 <74162594+zombiedude91@users.noreply.github.com>
2023-07-04 23:48:03 +0700

    update Text styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,29 @@ export default function Home() {
   return (
     <>
       <div className="h-screen bg-white">
+        {/* Top Bar */}
         <div className="bg-black text-white text-2xl px-5 py-3">New</div>
 
-        <div className="text-2xl px-36 py-16">
-          <h1 className="text-black font-bold text-5xl">
-            Hi, I'm
-          </h1>
-          <h1 className="text-black font-bold text-8xl mt-4">
-            Teerapat Satitporn.
-          </h1>
-          <h2 className="text-black font-bold text-5xl mt-48">
-            I am a web developer.
-          </h2>
+        {/* Content Container */}
+        <div className="container flex flex-row text-2xl px-36 py-16">
+          {/* Text */}
+          <div className="basis-11/12">
+            <h1 className="text-black font-bold text-8xl mt-4">
+              Teerapat Satitporn
+            </h1>
+            <h2 className="text-neutral-600 overline font-bold text-4xl mt-8">
+              Web Developer
+            </h2>
+
+            <p className="text-neutral-700 text-2xl font-medium mt-20">
+              Welcome to my digital domain, where the art of software development takes center stage,
+              seamlessly blending functionality, aesthetics, and flawless user experiences.
+            </p>
+          </div>
+
+          {/* Menu Bar */}
+          <div className="basis-1/12">
+          </div>
         </div>
       </div>
     </>
